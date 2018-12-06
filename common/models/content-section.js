186b1fdd7377edb0cc49ede493a90407bdd8e6cf
Diego Torres <diegotorres50@hotmail.com>
2018-12-05 19:18:57 -0500

    120518_mongo_datasource - adding emptor rest datasource and logic model

--- common/models/content-section.js
+++ common/models/content-section.js
@@ -1,5 +1,60 @@
-'use strict';
+'use strict'
 
-module.exports = function(Contentsection) {
+/**
+ * @module ContentSection
+ *
+ * ContentSection model contains all methods and attributes
+ * needed to management content sections persitence services.
+ *
+ * @version 1.0.0
+ * @see {@link https://loopback.io/doc/en/lb3/|LoopBack 3.x}
+ *
+ * @author Diego Torres <diecam@eltiempo.com>
+ */
+module.exports = function (ContentSection) {
+  /**
+   * remote method has to be registered
+   *
+   * @author Diego Torres <diecam@eltiempo.com>
+   *
+   * @param {string} remoteMethod - remote method name to register.
+   * @param {object} configuration to remote method.
+   */
+  ContentSection.remoteMethod(
+    'find',
+    {
+      description: 'Get info from sections.',
+      http: { path: '/get-all', verb: 'get' },
+      returns: { arg: 'result', type: 'object', root: true }
+    })
 
-};
+  /**
+   * remote method after hook
+   * @param {object} context - this is a request context.
+   * @param {object} unused - pending.
+   * @param {object} next - object.
+   * @return {string} result of the description function.
+   */
+  ContentSection.afterRemote('find', function (ctx, unused, next) {
+    if (ctx) {
+      ctx.result = {
+        code: 200,
+        message: 'Success',
+        data: ctx.result,
+        result: 'Okay'
+      }
+    } else {
+      next(new Error('Error'))
+    }
+    next()
+  })
+
+  /**
+   * @author Diego Torres <diecam@eltiempo.com>
+   */
+  ContentSection.observe('loaded', function logQuery (ctx, next) {
+    // Obtenemos la data del json y la transformamos
+    console.log(ctx.data)
+    next()
+  })
+}
---
+++ server/datasources.local.js
@@ -0,0 +1,38 @@
+// Copyright IBM Corp. 2014. All Rights Reserved.
+// Node module: loopback-example-offline-sync
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+module.exports = {
+  'db': {
+    'name': 'db',
+    'connector': 'memory'
+  },
+  'profiling_mongodb_ds': {
+    'host': 'localhost',
+    'port': 27017,
+    'url': 'mongodb://localhost:27017/contentProfiling',
+    'database': 'contentProfiling',
+    'password': '',
+    'name': 'profiling_mongodb_ds',
+    'user': '',
+    'connector': 'mongodb'
+  },
+  'emptor_rest_ds': {
+    'name': 'emptor_rest_ds',
+    'connector': 'rest',
+    'operations': [{
+      'functions': {
+        'getUserData': [
+          'ACCESS_TOKEN'
+        ]
+      },
+      'template': {
+        'method': 'GET',
+        'url': 'https://seg.eltiempobeta.com/v2/me',
+        'headers': {
+          'Authorization': 'Bearer ' + '{ACCESS_TOKEN}'
+        }
+      }
+    }]
+  }
+}
